2850a91c2a22d2e923842b20f0a3acae128b4ad0
Kevin Harriss <special.kevin@gmail.com>
2013-04-03 15:24:23 -0500

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "interrupted",
+    version = "0.0.1",
+    author = "Kevin Harriss",
+    author_email = "special.kevin@gmail.com",
+    description = ("A command-line tool to track your interruptions via StatHat"),
+    license = "BSD",
+    keywords = "commandline",
+    url = "http://github.com/specialkevin/interrupted",
+    packages=['interrupted',],
+    scripts=['bin/interrupted'],
+    classifiers=[
+                    "Development Status :: 2 - Pre-Alpha",
+                    "Topic :: Utilities",
+                    "License :: OSI Approved :: BSD License",
+                ],
+    install_requires = ['stathat'],
+)
