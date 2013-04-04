291ab79c520bf24836f7ed38de12eae35e01c4c2
Kevin Harriss <special.kevin@gmail.com>
2013-04-04 15:12:33 -0500

    Reverted back to normal and will just have to wait until pypi is updated.

--- setup.py
+++ setup.py
@@ -17,6 +17,5 @@ setup(
                     "Topic :: Utilities",
                     "License :: OSI Approved :: BSD License",
                 ],
-    install_requires = ['stathat>=0.0.3'],
-    dependency_links = ['https://github.com/kennethreitz/stathat.py/tarball/master#egg=stathat-0.0.3']
+    install_requires = ['stathat'],
 )
